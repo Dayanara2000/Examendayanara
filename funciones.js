bc92e0b631d88266aebc98ec456bc76b744445d7
unknown <estudiante@LAB0241.SERVERINT.LOCAL>
2017-03-10 12:08:51 -0600

    hola

---
+++ funciones.js
@@ -0,0 +1,8 @@
+var BAcoB=angular.module ("BacoB",[]):
+BacoB.controller ("mostrar",funtion ($scope){
+	$scope.Mensaje="dayanara"
+	$scope.operaciones=$scope.first+$scope.second;
+	$scope.first=16
+	$scope.second=3
+
+});
